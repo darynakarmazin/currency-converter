d394aa64b672592d78caa84286834d25e72a8ff5
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-12-20 16:33:30 +0200

    Updated project template.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import { Title } from "./Header.styles";
+
+function Header() {
+  return <Title>Currency Conversion</Title>;
+}
+
+export default Header;
