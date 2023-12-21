140ae660408967e31201403e93fec7f97cfede66
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-12-21 17:18:19 +0200

    Updated api key (because of free version)

--- src/servises/fetchCurrency.js
+++ src/servises/fetchCurrency.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 axios.defaults.baseURL = "https://api.apilayer.com/exchangerates_data/latest?";
-const access_key = "08WNZIF50bDbaNNw0FSTX7TbX5rVOV4V";
+// const access_key = "08WNZIF50bDbaNNw0FSTX7TbX5rVOV4V";
+const access_key = "34POXuV3iuyv6bYRNlflV0Dmq6cQ3I8O";
 
 export async function fetchExchange(base, symbols) {
   try {
